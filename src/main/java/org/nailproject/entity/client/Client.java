b6f42232ec96695c8264e93e097de18aed447834
Dmitrii Sagaiko <dmitsag@gmail.com>
2024-04-12 17:21:59 +0200

    validation on REST lvl

--- src/main/java/org/nailproject/entity/client/Client.java
+++ src/main/java/org/nailproject/entity/client/Client.java
@@ -1,6 +1,7 @@
 package org.nailproject.entity.client;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,16 +12,30 @@ import lombok.NoArgsConstructor;
 
 @Entity
 public class Client {
-
+    @NotNull(message = "firstName has to be assigned")
+    @Min(value = 5, message = "first name has to be longer then 5 symbols")
     private String firstName;
 
+
+    @NotNull(message = "lastName has to be assigned")
+    @Min(value = 5, message = "last name has to be longer then 5 symbols")
     private String lastName;
 
+
+    @Digits(integer = 3, fraction = 0, message = "Age has to be in range from 0 to 150")
+    @Max(value = 150, message ="Age has to be less then 150" )
+    @Positive
     private int age;
 
+    @Email
     private String email;
 
+    @Size(min = 9, max = 16)
+    @NotNull
+    @NotEmpty
     private String phoneNumber;
+
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
